741b74585673376a046cbf1e8e96560a247c1744
Angela Ale <angiale15@gmail.com>
2021-05-09 03:21:47 +0000

    Finish layout and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,2 @@
 class ApplicationController < ActionController::Base
-  
-   def test
-     render html: "Testing 1,2,3!"
-   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,13 @@
-# Returns the full title on a per-page basis. 
-def full_title(page_title = '')
-  base_title = "Edgeucation" 
-  if page_title.empty?
-    base_title
-  else
-    page_title + " | " + base_title
+module ApplicationHelper
+  
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "Edgeucation"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
   end
+
 end
