843deb06b3aaa445297a850d74e6b65e8c6b62df
Angela Ale <angiale15@gmail.com>
2021-06-04 19:09:51 +0000

    Changes to zipcode

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,7 +1,7 @@
 class Search < ApplicationRecord
   def search_colleges
     colleges = College.all 
-    colleges = colleges.where(['zip_code <= ? and zip_code >= ?', (zip_code.to_i+10000), (zip_code.to_i-10000).to_s]) if zip_code.present?
+    colleges = colleges.where(['zip_code <= ?', zip_code]) if zip_code.present?
     colleges = colleges.where(['cost_att <= ?', cost_att]) if cost_att.present?
 
     return colleges
