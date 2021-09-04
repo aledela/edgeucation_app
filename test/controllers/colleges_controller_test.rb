445d0dd63c3fde2270a9431e153e5309b7c94b87
Angela Ale <angiale15@gmail.com>
2021-09-04 02:52:06 +0000

    Last changes for debugging deployment and add favicon

--- test/controllers/colleges_controller_test.rb
+++ test/controllers/colleges_controller_test.rb
@@ -1,14 +1,14 @@
 require 'test_helper'
 
 class CollegesControllerTest < ActionDispatch::IntegrationTest
-  # setup do
-  #   @college = colleges(:one)
-  # end
+  setup do
+    @college = colleges(:one)
+  end
 
-  # test "should get index" do
-  #   get colleges_path
-  #   assert_redirected_to root_path
-  # end
+  test "should get index" do
+    get colleges_path
+    assert_redirected_to root_path
+  end
 
   # test "should get new" do
   #   get new_college_path
@@ -23,10 +23,10 @@ class CollegesControllerTest < ActionDispatch::IntegrationTest
   #   assert_redirected_to college_path(College.last)
   # end
 
-  # test "should show college" do
-  #   get college_path(@college)
-  #   assert_response :success
-  # end
+  test "should show college" do
+    get college_path(@college)
+    assert_response :success
+  end
 
   # test "should get edit" do
   #   get edit_college_path(@college)
